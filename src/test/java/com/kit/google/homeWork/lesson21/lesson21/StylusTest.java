907df4816ed43bd594950a1416d0a5fc057d5fef
katerynazora <kateryna.zora@gmail.com>
2017-06-08 11:24:09 +0300

    classWork

--- src/test/java/com/kit/google/homeWork/lesson21/lesson21/StylusTest.java
+++ src/test/java/com/kit/google/homeWork/lesson21/lesson21/StylusTest.java
@@ -1,10 +1,9 @@
-package com.kit.google;
+package com.kit.google.homeWork.lesson21.lesson21;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -12,38 +11,39 @@ import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertTrue;
 
-
 /**
- * Created by Kateryna Zora  on 02.06.2017.
+ * Created by Kateryna Zora  on 06.06.2017.
+ *
  */
-public class GoogleSearchTest {
+public class StylusTest {
 
     private String googleSearch;
     private WebDriver webDriver;
 
     @BeforeClass
     public void setUp(){
-       System.setProperty("webdriver.chrome.driver", "D:\\IdeaProjects\\src\\main\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\IdeaProjects\\src\\main\\resources\\chromedriver.exe");
         webDriver = new ChromeDriver();
-        googleSearch = "https://www.google.com.ua/";
+        googleSearch = "http://www.stylus.com.ua/";
         webDriver.get(googleSearch); // открывает страницу
         webDriver.manage().window().maximize(); // открывает на всю страницу
         webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); // неявное ожидание
     }
 
     @Test
-    public void searchTest(){
-        String searchText = "Selenium";
-        By searchLocator = (By.cssSelector("#lst-ib")); // куда отправлять данные
-//        By searchLocator = (By.name("q")); // куда отправлять данные
+    public void stylusTest(){
+        String searchText2 = "Sony Xperia Z2";
+        By searchLocator = (By.cssSelector("#head-search>form>input")); // куда отправлять данные
         WebElement searchField = webDriver.findElement(searchLocator); // переходит по атрибуту (в веб елемент)
-        searchField.sendKeys(searchText);
+        searchField.sendKeys(searchText2);
         searchField.submit(); // отправка данных (Enter)
-        By linkLocator = By.xpath(".//*[@id='rso']/div[2]/div/div[1]/div/div/h3/a"); // по xpath в FirePath
+        By linkLocator = By.xpath(".//*[@id='search-list']/ul/li[1]/a/span"); // по xpath в FirePath
         WebElement link = webDriver.findElement(linkLocator);
-        assertTrue(link.getText().contains(searchText)); // верефецируем, что это точно первая ссылка
-
-
+        assertTrue(link.getText().contains(searchText2)); // верефецируем, что это точно первая ссылка
+        link.click();
+        By headerLocator = By.xpath(".//*[@id='product-block']/header/div[1]/h1");
+        WebElement header = webDriver.findElement(headerLocator);
+        assertTrue(header.getText().contains(searchText2));
     }
 
 }
---
+++ src/test/java/com/kit/google/homeWork/lesson21/lesson22/EnterMailTest.java
@@ -0,0 +1,8 @@
+package com.kit.google.homeWork.lesson21.lesson22;
+
+/**
+ * Created by Kateryna Zora  on 08.06.2017.
+ */
+public class EnterMailTest {
+
+}
